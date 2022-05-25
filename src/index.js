daccbc2b22ef4cff8c7db76899683f1b6ac48429
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-25 10:23:37 +0000

    Add css stylesheet inport

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css";
-import "./index.css";
+import "./assets/css/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
