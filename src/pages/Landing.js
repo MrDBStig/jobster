9a1f3984c90469b96a00f9e7dad9238fab28b7e7
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-25 12:11:35 +0000

    Move Wrapper to a separate file

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
@@ -27,42 +27,5 @@ const Landing = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: repeat(2, 1fr);
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
