d4f8f0cff9efbec60bda20b0962004d683a6c6b5
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-26 17:11:03 +0000

    Create allJobsSlice

---
+++ src/features/allJobs/allJobsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import customFetch from "../../utils/axios";
+import { toast } from "react-toastify";
+
+const initialFiltersState = {
+  search: "",
+  searchStatus: "all",
+  searchType: "all",
+  sort: "latest",
+  sortOptions: ["latest", "oldest", "a-z", "z-a"],
+};
+
+const initialState = {
+  isLoading: false,
+  jobs: [],
+  totalJobs: 0,
+  numOfPages: 1,
+  page: 1,
+  stats: {},
+  monthlyApplications: [],
+  ...initialFiltersState,
+};
+
+const allJobsSlice = createSlice({
+  name: "allJobs",
+  initialState,
+});
+
+export default allJobsSlice.reducer;
