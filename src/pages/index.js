e1ae528b3ac266481855c4956056c42ea2beca5a
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-26 09:27:35 +0000

    Create nested dashboard structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Landing from "./Landing";
-import Dashboard from "./Dashboard";
 import Register from "./Register";
 import Error from "./Error";
 
-export { Landing, Error, Register, Dashboard };
+export { Landing, Error, Register };
