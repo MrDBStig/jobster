b0c77eeca9656e3f989958428afba51220f8cf48
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-26 10:02:26 +0000

    Create Navbar logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Logo from "./Logo";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle logout dropdown")}
+          >
+            <FaUserCircle /> {user?.name} <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout user")}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
