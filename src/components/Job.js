bd86a0811007c0da40aaa0a4e10e28276e53c104
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-26 19:43:12 +0000

    Create JobInfo component

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,7 +1,62 @@
-import React from "react";
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import JobInfo from "./JobInfo";
+import Wrapper from "../assets/wrappers/Job";
 
-const Job = () => {
-  return <div>Job</div>;
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  status,
+}) => {
+  const dispatch = useDispatch();
+  const date = createdAt;
+
+  return (
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className="actions">
+            <Link
+              to="/addJob"
+              className="btn edit-btn"
+              onClick={() => {
+                console.log("edit job");
+              }}
+            >
+              Edit
+            </Link>
+            <button
+              type="button"
+              className="btn delete-btn"
+              onClick={() => {
+                console.log("delete job");
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Job;
