6c87a97a3feaf6cbbdf26dfb3e35815fe6e4f1d1
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-26 10:49:10 +0000

    Complete small sidebar functionality

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
-  return <h1>SmallSidebar</h1>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div className={`sidebar-container ${isSidebarOpen && "show-sidebar"}`}>
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
