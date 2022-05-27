28d94cc25c903bd53988bc991397d0db4bc94fae
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-05-27 14:41:03 +0000

    Create StatItem component

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,7 +1,15 @@
-import React from "react";
+import Wrapper from "../assets/wrappers/StatItem";
 
-const StatItem = () => {
-  return <div>StatItem</div>;
+const StatItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatItem;
